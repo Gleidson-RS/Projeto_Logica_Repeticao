fdc4bf6192f432a9bedcb3bf3b651e9980eb39ad
gleidson <gleidsonrosasantos2@gmail.com>
2026-06-16 16:39:40 -0300

    começo do codigo

--- js/scripts_repeticoes.js
+++ js/scripts_repeticoes.js
@@ -3,4 +3,11 @@ const divFor = document.querySelector('#div-for')
 
 for(i = 0;i < 10; i++ ){
     console.log(i, 'Amor')
+    divFor.innerHTML += `${i} - Amor <br>`
 }
+
+//CONTADOR/ACUMULADOR 
+const inputNum = document.querySelector('Num')
+const btnNum = document.querySelector('#btn-num')
+const divContAcum = document.querySelector('#div-cont-acum')
+
