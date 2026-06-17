5053f6fc516f4a67bb5b1dc9a48af792562ec2cf
gleidson <gleidsonrosasantos2@gmail.com>
2026-06-17 16:28:00 -0300

    codigo conteudo em andamento - for, array, for in, for of, for aech e

--- js/scripts_repeticoes.js
+++ js/scripts_repeticoes.js
@@ -69,3 +69,12 @@ presentes.forEach((elemento, posicao) =>{
     divForeach.innerHTML += `${posicao} - ${elemento} <br>`
 })
 
+
+const pessoas = [
+    {nome: 'Maria Flor', idade: 25, renda:1800},
+    {nome: 'Joerdson Souza', idade: 75, renda:5000},
+    {nome: 'Taoca', idade: 28, renda:500},
+    {nome: 'Chicó', idade: 36, renda:100},
+    {nome: 'João Grilo', idade: 36, renda:80},
+
+]
