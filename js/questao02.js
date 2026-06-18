6729655c2d06a5eeb456b3c7ca518e832f47d6b9
gleidson <gleidsonrosasantos2@gmail.com>
2026-06-18 15:28:26 -0300

    atividade 02 finalizado

--- js/questao02.js
+++ js/questao02.js
@@ -1,3 +1,24 @@
+const inputdivnum = document.querySelector('#num1')
+const divNum = document.querySelector('#div-num')
+
+document.querySelector('form').addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    let Num = Number(inputdivnum.value)
+
+    console.log(Num)
+
+
+for(i = 0;i < Num; i++){
+    if(Num % i == 0){
+        console.log(Num)
+        divNum.innerHTML +=  `${i} <br>`
+    }
+}
+
+})
+
+
 
 
 
