2aa568592bac93b18d0caccfaf12e608ff685828
Tarde Cetafaju <Tarde.Cetafaju@labfies.org.br>
2026-06-19 14:59:33 -0300

    aula de funçoes

--- js/scripts_funcoes.js
+++ js/scripts_funcoes.js
@@ -31,3 +31,69 @@ const msgArrow = () => {
 }
 
 msgArrow()
+
+const divisaoNum = (valor1, valor2) => {
+
+let respostas
+
+    if(valor1 > 0 && valor2 > 0){
+        respostas = valor1 / valor2
+    }else{
+    respostas = 'não é possivel fazer uma divisão por 0'
+    }
+
+return respostas
+
+}
+
+console.log(divisaoNum(10,2))
+
+console.log(Math.random())
+
+console.log('Numero Sorteado', parseInt(Math.random() * 100))
+/*
+
+setTimeout(() => {
+    console.log('Executou apos 5s')
+    console.log('Numero Sorteado 2', parseInt(Math.random() * 100))
+}, 5000);
+
+let cont = 0
+
+const intervalo = setInterval(() => {
+    cont++
+    console.log(cont, 'Repetição de setInterval')
+
+
+if (cont == 5){
+    clearInterval(intervalo)
+}
+
+}, 3000)
+*/
+
+
+let contVolta = 0, numSorteado = 0
+
+const sorteo = setInterval(() => {
+    contVolta++ 
+
+numSorteado = parseInt(Math.random() * 10)
+
+console.log(`${contVolta}º número sorteado ${numSorteado}`)
+
+divNumSorteio.innerHTML += (`${contVolta}º número sorteado ${numSorteado} <br>`)
+
+
+if (numSorteado == 9){
+    console.log('SORTEIO INCERRADO')
+    clearInterval(sorteo)
+    contVolta = 0
+    numSorteado = 0
+
+
+}
+}, 2);
+
+const divNumSorteio = document.querySelector('#div-num-sorteio')
+
