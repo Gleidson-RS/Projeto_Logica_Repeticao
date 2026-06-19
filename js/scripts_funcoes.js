54f9158578cd06e46184523225117b32ec4532fd
Tarde Cetafaju <Tarde.Cetafaju@labfies.org.br>
2026-06-19 14:07:17 -0300

    aula de funçoes

---
+++ js/scripts_funcoes.js
@@ -0,0 +1,26 @@
+
+function mensagem() {
+    console.log('Uma mensagem')
+}
+
+mensagem()
+
+function mensagemlinda(txt) {
+    console.log(txt)
+}
+
+mensagemlinda('Brasil vai dar um pau no Haiti')
+
+function somaNumeros(num1, num2) {
+    return num1 + num2
+}
+
+console.log('A soma dos numeros ', somaNumeros(10, 58))
+
+const multiplicacao = function (num1, num2) {
+    return num1 * num2
+}
+
+console.log('A multiplicação dos numero ', multiplicacao(64, 5))
+
+
